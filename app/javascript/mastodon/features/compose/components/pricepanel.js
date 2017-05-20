8a4e2b657042c4fa4c26262d75c5f1ddbbfe5b6c
dogwood008 <dogwood008+github@gmail.com>
2017-05-20 22:08:03 +0900

    :+1: add price panel

--- app/javascript/mastodon/features/compose/components/pricepanel.js
+++ app/javascript/mastodon/features/compose/components/pricepanel.js
@@ -1,41 +1,11 @@
 import React from 'react';
-import ImmutablePropTypes from 'react-immutable-proptypes';
-import ImmutablePureComponent from 'react-immutable-pure-component';
-import Heartbeat from '../heartbeat';
-import socket from '../socket';
 
-class Pricepanel extends ImmutablePureComponent {
-  constructor(props){
-    super(props);
-    this.state = {
-      heartbeat: undefined
-    }
+export default class Pricepanel extends React.Component {
+  render(){
+    return(
+        <div className="pricepanel">
+          <iframe scrolling="no" allowTransparency="true" frameBorder="0" width="100%" height="100%" src="/pricepanel.htm"></iframe>
+        </div>
+        );
   }
-
-  componentDidMount() {
-    socket.on('news', (data) => {
-      let json = JSON.parse(data)
-      if(typeof json.heartbeat !== 'undefined'){
-        this.receiveHeartbeat(json);
-      }
-    });
-  }
-
-  receiveHeartbeat(message) {
-    if(process.env.NODE_ENV === 'development') {
-      console.log(message.heartbeat.time);
-    }
-    this.setState({ heartbeat: message.heartbeat.time });
-  }
-
-  render () {
-    return (
-      <div id='fx_pricepanel'>
-        <Heartbeat time={this.state.heartbeat || 'undefined'} />
-      </div>
-    );
-  }
-
 }
-
-export default Pricepanel;
